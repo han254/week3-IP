b382199d0ea96188c56c9b199c1fc8a245cae5a5
han254 <peternjenga1706@gmail.com>
2022-02-06 20:24:11 +0300

    build:compare user's with correct answers

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ userForm.addEventListener("submit", (e) => {
   correctAnswer(userData);
 });
 
-// // compare user's with correct answers
-// function correctAnswer(userData) {
-//   for (let answer of userData) {
-//     var userKey = answer[0];
-//     var userValue = answer[1];
-//     var correctKey = quizData[1];
-//     console.log(answer);
-//   }
-// }
+//  compare user's with correct answers
+function correctAnswer(userData) {
+  for (let answer of userData) {
+    var userKey = answer[0];
+    var userValue = answer[1];
+     var correctKey = quizData[1];
+     console.log(answer);
+ }
+}
 // // update score on correct answers
 // // display the score to user
