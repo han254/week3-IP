bf37d102331b7bfa2fef50c5ce084c07aa5acc0a
han254 <peternjenga1706@gmail.com>
2022-02-06 10:07:33 +0300

    refactor: changing the questions layout

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const quizData =[
+    {
+        quiz: "If we don't want to allow a floating div to the left side of an element, which css property will we use ?",
+        a: "padding",
+        b: "margin",
+        c: "float",
+        d: "clear",
+        correct: "d",
+  },
+
+]
